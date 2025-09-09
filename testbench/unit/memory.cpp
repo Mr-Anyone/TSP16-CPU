0cfaedc9a0b02afbf5c6617f6dc7ddefecafa55c
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2025-09-08 17:26:12 -0700

    Added testcase for 16 bit memory address.

--- testbench/unit/memory.cpp
+++ testbench/unit/memory.cpp
@@ -1,7 +1,12 @@
 #include <verilated.h>
 #include <verilated_vcd_c.h>
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+
 #include "VMemory.h"
+
+// FIXME: fix this jankness, this really should be some fixture instead of global variable
 
 VerilatedContext* init(){
     Verilated::traceEverOn(true); // trace is on now
@@ -12,19 +17,11 @@ VerilatedContext* init(){
     return  context;
 }
 
-int main(){
-    VerilatedContext* context = init();
-    VMemory *dut = new VMemory(context);
-
-    // Create the waveform for the top level module
-    VerilatedVcdC *m_trace = new VerilatedVcdC;
-    dut->trace(m_trace, /*levels=*/5);
-    m_trace->open("memory-waveform.vcd");
-
+void write_mem(VMemory* dut, VerilatedVcdC* m_trace, VerilatedContext* context, uint16_t address, uint16_t content){
     dut->clk = 0;
     dut->write = 1;
-    dut->write_address = 0x0; 
-    dut->write_input = 10;
+    dut->write_address = address; 
+    dut->write_input = content;
     dut->eval(); 
     m_trace->dump(context->time());
     context->timeInc(1); 
@@ -33,15 +30,45 @@ int main(){
     dut->eval(); 
     m_trace->dump(context->time());
     context->timeInc(1); 
+}
 
-    dut->clk = 0; 
-    dut->read_address = 0x0;
+uint16_t read_mem(VMemory* dut, VerilatedVcdC* m_trace, VerilatedContext* context, uint16_t address){
+    dut->read_address = address;
     dut->eval(); 
-    if(dut->read_output != 10){
-        std::cout << "test has failed" << std::endl;
-    }
+    uint16_t result = dut->read_output;
     m_trace->dump(context->time());
-    context->timeInc(1); 
+    context->timeInc(1);
+
+    return result;
+}
+
+int main(){
+    VerilatedContext* context = init();
+    VMemory *dut = new VMemory(context);
+    constexpr int size = 65536;
+
+    // Create the waveform for the top level module
+    VerilatedVcdC *m_trace = new VerilatedVcdC;
+    dut->trace(m_trace, /*levels=*/5);
+    m_trace->open("memory-waveform.vcd");
+    std::vector<uint16_t> mem(size, 0);
+    
+    // FIXME: don't hardcode this size!
+    for(int i = 0;i<size; ++i){
+        uint16_t result = (uint16_t)(rand()); 
+        mem[i] = result;
+        write_mem(dut, m_trace, context, i, mem[i]);
+        uint16_t read = read_mem(dut, m_trace, context, i);
+        if(read != mem[i]){
+            std::cout << "test have failed" << std::endl; 
+        }
+    }
+
+    for(int i = 0;i<size; ++i){
+        if(read_mem(dut, m_trace, context, i) != mem[i]){
+            std::cout << "test have failed" << std::endl; 
+        }
+    }
 
     m_trace->close(); 
 
